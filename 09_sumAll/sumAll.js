871896ef133c45ab65b33b171b0fb31d9ed2af07
Yasin Atıcı <yasinatici273@gmail.com>
2025-05-12 22:52:23 +0300

    9. egzersiz çözüldü
    Number.isInteger(value) öğrenildi

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    let total = 0;
+    let smallNumber;
+    let bigNumber; 
 
+    if(number1<0 || number2<0 || !Number.isInteger(number1) || !Number.isInteger(number2)){
+        return "ERROR";
+    }
+
+    if(number1 < number2) {
+        smallNumber = number1;
+        bigNumber = number2;
+    } else {
+        smallNumber = number2;
+        bigNumber = number1;
+    }
+
+    for(let i = smallNumber; i <= bigNumber; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line

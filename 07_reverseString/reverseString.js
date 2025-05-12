4d7518956e44a0ea2b9eaabfdf45235138358b31
Yasin Atıcı <yasinatici273@gmail.com>
2025-05-12 15:51:09 +0300

    7. egzersiz çözüldü
    reverse string sorusu çözüldü

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(obj) {
+    let arr = obj.toString().split('');
+    let reversedArr = arr.reverse();
+    return reversedArr.join('');
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;

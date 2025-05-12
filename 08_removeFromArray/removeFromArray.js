5a085db407826062c25e32c6c52d7b8abc506d67
Yasin Atıcı <yasinatici273@gmail.com>
2025-05-12 22:39:48 +0300

    8. egzersiz çözüldü
    filter methodu ve ...args  kullanımı anlaşıldı

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+   return arr.filter(item => !args.includes(item))
 };
+
+// ([1, 2, 3, 4], 3, 2)
 
 // Do not edit below this line
 module.exports = removeFromArray;
